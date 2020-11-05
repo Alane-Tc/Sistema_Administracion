ed16a8a4067f2b3a20b28f3da97ecb7d672d58bb
Rocio Aguirre <rocio.aguirre.ba@gmail.com>
2020-11-05 16:07:59 -0600

    This is a patient commit

--- src/tecmilenio/alantamez/Patient.java
+++ src/tecmilenio/alantamez/Patient.java
@@ -4,7 +4,25 @@ public class Patient {
     //Patient Class
 
     //Atributos
+    private int id_Patient;
+    private String patient_Name;
+    private String lastName;
+    private int age;
+    private String date_Birth;
+    private String gender;
+    private String adress;
 
     //Metodos
+    private void show_result_patient(){
 
+    }
+    private void add_patient(){
+
+    }
+    private void remove_patient(){
+
+    }
+    private void update_patient(){
+
+    }
 }
