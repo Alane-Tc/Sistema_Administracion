97ebdca7c4d5ba6f29ef2e9bb07b4699a1b4dcd2
Mitzi Cabrera <Mitzi0107@outlook.com>
2020-11-05 16:22:44 -0600

    cambios de clase date

--- src/tecmilenio/alantamez/Date.java
+++ src/tecmilenio/alantamez/Date.java
@@ -1,5 +1,26 @@
 package tecmilenio.alantamez;
 
+//This class Date
+
 public class Date {
-    // futuro codigo :3 <3
+    private int id_date;
+    private  int Doctor_id;
+    private int Patient_id;
+    private String Date;
+    private String Time;
+    private String Reason_date;
+
+    public void ShowDate () {
+
+    }
+
+    public void AddDate () {
+
+    }
+
+    public void RemoveDate () {
+
+
+    }
+
 }
