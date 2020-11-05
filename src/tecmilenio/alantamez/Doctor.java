ac80ee1d2914cce11a0d4637944f849c5a4dc39c
Jose_Martinez <al0277334@tecmilenio.mx>
2020-11-05 15:37:21 -0600

    This is a doctor commit

--- src/tecmilenio/alantamez/Doctor.java
+++ src/tecmilenio/alantamez/Doctor.java
@@ -1,10 +1,24 @@
 package tecmilenio.alantamez;
+//This is the doctor class
 
 public class Doctor {
-    //This is the doctor class
+
     //atributes
-    
+    private String nombre_doctor;
+    private int id_doctor;
+    private String contraseña_admin;
 
     //methods
+    private void  Show_Results(){
+        //in here goes the showing results code
+    }
+
+    private void Remove_Results(){
+        //in here goes the remove results code
+    }
+
+    private void Add_results(){
+        //in here goes the add results code
+    }
 
 }
