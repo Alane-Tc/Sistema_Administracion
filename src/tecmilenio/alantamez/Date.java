3bd0e9aa8d6fed1df2fcb8a1c3585d0b985e1cb5
Chio145 <rocio.aguirre.ba@gmail.com>
2020-11-28 21:06:59 -0600

    Modificacion de metodos en la clase Date

--- src/tecmilenio/alantamez/Date.java
+++ src/tecmilenio/alantamez/Date.java
@@ -2,7 +2,10 @@ package tecmilenio.alantamez;
 
 //This class Date
 
+import java.util.Scanner;
+
 public class Date {
+    private Scanner ent = new Scanner(System.in);
     private int id_date;
     private  int Doctor_id;
     private int Patient_id;
@@ -15,15 +18,25 @@ public class Date {
     Files fileDate = new Files();
 
     public void NumCitas () {
-
+        System.out.print("¿Cuantos citas desea registrar?: ");
+        ncitas = ent.nextInt();
     }
 
     public void datosCitas () {
-
+        System.out.print("¿Cuál es el id de la cita?: ");
+        id_date = ent.nextInt();
+        System.out.print("¿Cuál es la fecha de la cita?: ");
+        Date = ent.next();
+        System.out.print("¿A que hora es la cita?: ");
+        Time = ent.next();
+        System.out.print("¿Cuál es el motivo de la cita?: ");
+        Reason_date = ent.next();
     }
 
     public void opcionCitas () {
-
+        System.out.println(
+                "\n Selecciona una opción" + "\n 1- Alta Cita" +
+                        "\n 2-MostrarCitas"+ "\n 3- Baja Cita");
     }
 
     public void altaCita(){
